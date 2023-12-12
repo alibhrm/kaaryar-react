e7c33a74e81d44e8bdd34a3ea3c78201f495d32d
alibhr <mbahrololumi10@gmail.com>
2023-12-12 21:51:42 +0330

    bakhsh 2 tamrin anjam va ezafe shod

--- tamrin2/src/App.js
+++ tamrin2/src/App.js
@@ -1,6 +1,93 @@
 import React from "react";
 import "./App.css";
 import users from "./users.json";
+import inputs from "./form.json"
+const renderInput = (input) => {
+  switch (input.type) {
+    case "text":
+      return (
+        <label htmlFor="">
+          {input.label}
+        <input
+          type="text"
+          name={input.name}
+          id={input.id}
+          placeholder={input.placeholder}
+          required={input.required}
+        />
+        </label>
+      );
+    case "password":
+      return (
+        <label htmlFor="">{input.label}
+        <input
+          type="password"
+          name={input.name}
+          id={input.id}
+          placeholder={input.placeholder}
+          required={input.required}
+        />
+        </label>
+      );
+    case "email":
+      return (
+        <label htmlFor="" >
+          {input.label}
+        <input
+          type="email"
+          name={input.name}
+          id={input.id}
+          placeholder={input.placeholder}
+          required={input.required}
+        />
+        </label>
+      );
+    case "select":
+      return (
+        <label htmlFor="">
+          {input.label}
+        <select name={input.name} id={input.id} required={input.required}>
+          {input.options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+        </label>
+      );
+    case "radio":
+      return (
+        <label htmlFor="">
+          {input.label}
+        <div className="gender">
+          {input.options.map((option) => (
+            <label>
+              {option.label}
+            <input
+              type="radio"
+              name={input.name}
+              value={option.value}
+              key={option.value}
+              label={option.label}
+            />
+            </label>
+          ))}
+        </div>
+        </label>
+      );
+    default:
+      return null;
+  }
+};
+const renderForm = (inputs) => {
+  return (
+    <div className="form">
+      {inputs.map((input) => (
+        renderInput(input)
+      ))}
+    </div>
+  );
+};
 
 const App = () => {
   const renderUser = (user) => {
@@ -10,7 +97,7 @@ const App = () => {
         <div className="user-name" style={{ color: color }}>
           {user.first_name}
         </div>
-      </div>
+        </div>
     );
   };
 
@@ -18,7 +105,13 @@ const App = () => {
     return <div className="users">{users.map((user) => renderUser(user))}</div>;
   };
 
-  return <div className="app">{renderUsers()}</div>;
+  return (<div className="app">
+    {renderUsers()}
+    <div className="form">
+       <h1>Form</h1>
+       {renderForm(inputs)}
+     </div>
+    </div>);
 };
 
 export default App;
